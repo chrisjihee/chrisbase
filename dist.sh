e6a32d9e361b6e4f4db866eac83bfe24997c5714
Jihee Ryu <chrisjihee@etri.re.kr>
2023-12-08 15:53:17 +0900

    Update dist.sh

--- dist.sh
+++ dist.sh
@@ -1,10 +1,10 @@
-conda create --name chrisbase python=3.9 -y; conda activate chrisbase;
+mamba create --name chrisbase python=3.9 -y; mamba activate chrisbase;
 rm -rf build dist src/*.egg-info;
 pip install build; python3 -m build;
 pip install twine; python3 -m twine upload dist/*;
 rm -rf build dist src/*.egg-info;
 
 sleep 3; clear;
-conda create --name chrisbase python=3.9 -y; conda activate chrisbase;
+mamba create --name chrisbase python=3.9 -y; mamba activate chrisbase;
 sleep 5; clear;
 pip install --upgrade chrisbase; pip list;
