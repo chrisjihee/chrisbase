084865d84c7b77631cd6035202c251d85b36e303
Jihee Ryu <chrisjihee@etri.re.kr>
2024-05-12 06:09:26 +0900

    Update dist.py: 3.9 -> 3.11

--- dist.sh
+++ dist.sh
@@ -1,10 +1,10 @@
-mamba create --name chrisbase python=3.9 -y; mamba activate chrisbase;
+mamba create --name chrisbase python=3.11 -y; mamba activate chrisbase;
 rm -rf build dist src/*.egg-info;
 pip install build; python3 -m build;
 pip install twine; python3 -m twine upload dist/*;
 rm -rf build dist src/*.egg-info;
 
 sleep 3; clear;
-mamba create --name chrisbase python=3.9 -y; mamba activate chrisbase;
+mamba create --name chrisbase python=3.11 -y; mamba activate chrisbase;
 sleep 5; clear;
 pip install --upgrade chrisbase; pip list;
