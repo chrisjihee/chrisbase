c3196ddc5f16aac6cdfc397c7db2a4406b599b29
Jihee Ryu <chrisjihee@etri.re.kr>
2025-03-28 14:50:09 +0900

    Add run_command()

--- src/chrisbase/cli.py
+++ src/chrisbase/cli.py
@@ -1,6 +1,17 @@
+import subprocess
+
 from typer import Typer
 
 app = Typer()
+
+
+def run_command(cmd):
+    cmd = cmd.strip().split()
+    print("*" * 120)
+    print("[COMMAND]", " ".join(cmd))
+    print("*" * 120)
+    subprocess.run(cmd)
+    print("\n" * 3)
 
 
 @app.command()
