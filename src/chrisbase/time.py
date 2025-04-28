8e1a5117007cfd78515594830d3287ef5b290e88
Jihee Ryu <chrisjihee@etri.re.kr>
2025-04-29 06:12:27 +0900

    run_on_local_main_process() -> run_only_rank_zero(), run_only_main_process()

--- src/chrisbase/time.py
+++ src/chrisbase/time.py
@@ -60,10 +60,22 @@ def wait_for_everyone():
 
 
 @contextmanager
-def run_on_local_main_process(local_rank: int = int(os.getenv("LOCAL_RANK", -1))):
+def run_only_rank_zero(rank: int = int(os.getenv("LOCAL_RANK", -1))):
     wait_for_everyone()
     try:
-        if local_rank == 0:
+        if rank == 0:
+            yield
+        else:
+            yield None
+    finally:
+        wait_for_everyone()
+
+
+@contextmanager
+def run_only_main_process(is_main_process: bool = True):
+    wait_for_everyone()
+    try:
+        if is_main_process:
             yield
         else:
             yield None
